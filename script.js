9f7c185213d7574e6c3a8445ed7108e8468a7a5a
9d662zvzjq-collab <9d662zvzjq@privaterelay.appleid.com>
2026-06-19 03:58:35 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage(){
+document.getElementById("msg").innerHTML =
+"يا أمونتي ❤️<br><br>" +
+"إنتي شخص مهم جدًا، ووجودك بيفرق فعلاً ✨<br>" +
+"ربنا يفرح قلبك دايمًا ويخليكي مبسوطة ❤️";
+}
